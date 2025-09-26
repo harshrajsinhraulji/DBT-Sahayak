46ae12d11ba4033f0e6dc10c426882d0a3c15bf9
Harshrajsinh Raulji <rauljiharsh724@gmail.com>
2025-09-26 23:10:55 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase/plugin';
+import {firebase} from '@genkit-ai/firebase';
 import 'dotenv/config';
 
 export let ai = genkit({
