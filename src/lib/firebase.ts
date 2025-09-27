8adac357dd43445319396f6aa2206081cf0e755e
Harshrajsinh Raulji <rauljiharsh724@gmail.com>
2025-09-27 12:29:57 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).     at cr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,16 @@
-
 // This file is machine-generated - edit with care!
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBUjfBxveru9rBX_6wyOQvGY5dNeZ3Vx4c",
+  authDomain: "dbt-sahayak-database.firebaseapp.com",
+  projectId: "dbt-sahayak-database",
+  storageBucket: "dbt-sahayak-database.firebasestorage.app",
+  messagingSenderId: "36334179231",
+  appId: "1:36334179231:web:5fd14a0515801ec918a3aa",
+  measurementId: "G-QMET9YF77M"
 };
 
 
@@ -20,4 +19,3 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
-
