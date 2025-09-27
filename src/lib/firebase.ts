8d968ecc06a3aee31b29a15b1cbae3bb7a8fe5ec
Harshrajsinh Raulji <rauljiharsh724@gmail.com>
2025-09-27 12:14:36 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,19 @@
+
 // This file is machine-generated - edit with care!
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "studio-353781992-6bde9",
-  "appId": "1:836635552483:web:45516c721bcfb709cbb3d6",
-  "apiKey": "AIzaSyDfhTS63lp7cfUEDykXo9ka8v0TakjmmBI",
-  "authDomain": "studio-353781992-6bde9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "836635552483"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
